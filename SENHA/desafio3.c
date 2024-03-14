2b537ceabb556a304fec048c478e7c9a1f9da8fe
Pietro <pietro.rosolia.2704@gmail.com>
2024-03-14 12:50:27 -0300

    SEPARANDO

---
+++ SENHA/desafio3.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	int senha, chute;
+	int vidas = 3;
+	
+	printf("1 - Digite a senha: ");
+	scanf("%i", &senha);
+
+	system("cls");
+	
+	while(vidas>0){
+		
+		printf("\n2 - Qual é a senha?  ");
+		scanf("%i", &chute);
+		
+		if(senha == chute){
+			printf("Acertou");
+			break;
+		}
+		else{
+			vidas--;
+			if (vidas == 0){
+				printf("VC perdeu");
+				return 0;
+			}
+			printf("\nTente novamente, vc tem %i vidas: ", vidas);			
+			
+		}
+	
+	}
+	
+	
+		
+	
+	return 0;
+}
