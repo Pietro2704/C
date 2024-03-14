0bf86a93b67f92613f96646537115f85ae96557b
Pietro Rosolia <102825626+Pietro2704@users.noreply.github.com>
2024-03-14 09:53:45 -0300

    commitado

---
+++ aula.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+		
+	int inicio;
+	printf("Digite o comeco: ");
+	scanf("%i", &inicio);
+	
+	int fim;
+	printf("Digite o fim: ");
+	scanf("%i", &fim);
+	
+	int i;
+	for(i=inicio; i<=fim; i++){
+		if(i%2==1){
+			printf("\n%i", i);
+		}
+	}
+	
+	
+}
