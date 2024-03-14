c02d1e75d5c3ba15e145d5a061aa568424bf9090
Pietro Rosolia <102825626+Pietro2704@users.noreply.github.com>
2024-03-14 10:14:55 -0300

    recursividade

---
+++ desafio2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Recursividade
+void num(int inicio, int fim){
+    
+    if(inicio<=fim){
+        
+        if(inicio%2==1){
+            
+        printf("%i\n",inicio);
+        
+        }
+        
+        inicio++;
+        num(inicio,fim);
+    }
+}
+
+main(){
+		
+	int inicio;
+	printf("Digite o comeco: ");
+	scanf("%i", &inicio);
+	
+		
+	int fim;
+	printf("Digite o fim: ");
+	scanf("%i", &fim);
+	
+    //	int i;
+    //	for(i=inicio; i<=fim; i++){
+    //		if(i%2==1){
+    //			printf("\n%i", i);
+    //		}
+    //	}
+	
+    num(inicio,fim);
+}
