1b14be55a3bae56b8cdb40ff3e44beb377d60da6
Pietro Rosolia <102825626+Pietro2704@users.noreply.github.com>
2024-03-21 10:48:23 -0300

    varios ja

--- desafios/desafio.c
+++ desafios/desafio.c
@@ -25,9 +25,15 @@ main(){
 }
 */
 
+
+
 // Fazer um programa que force o usuário a digitar um número par para finalizar o programa. 
 // Enquanto o usuário estiver digitando um número ímpar
 // o programa fica repetindo a mensagem "Digite um número par para finalizar".
+
+
+
+
 
 // Faça um programa que exiba a tabuada até 10 de um número digitado pelo usuário.
 /*
@@ -37,39 +43,77 @@ main(){
 
     printf("Tabuada: ");
     scanf("%i", &num);
-    
-	printf("\nInício: ");
-	scanf("%i", &init);
-
-	printf("\nFim: ");
-	scanf("%i", &fim);
 	
 	system("cls");
-	
-	if(init > fim){
-		
-		for(i = init; i >= fim; i--){
-    		result = num * i;
-    		printf("%i  X  %i  =  %i\n", num, i, result);
-		}
-		
-	}else{
-	
-		for(i = init; i <= fim; i++){
-    		result = num * i;
-    		printf("%i  X  %i  =  %i\n", num, i, result);
-		}
-		
+
+	for(i = 1; i <= 10; i++){
+		result = num * i;
+		printf("%i  X  %i  =  %i\n", num, i, result);
 	}
+	
 
     system("pause");
 
 }
 */
 
-// Faça um programa que peça para o usuário digitar um número de acordo com o número digitado, o programa deve exibir a frase bom dia "n" vezes, conforme o número digitado pelo usuário.
+
+
+
+
+// Faça um programa que peça para o usuário digitar um número,
+// de acordo com o número digitado, o programa deve exibir a frase bom dia "n" vezes, conforme o número digitado pelo usuário.
+/*
+main(){
+    int num, i;
+    setlocale(LC_ALL,"Portuguese");
+
+    printf("Digite um número: ");
+    scanf("%i", &num);
+    
+	for(i = 0; i<=num; i++){
+		printf("\nBom dia!");
+	}
+
+}
+*/
+
+
+
+
 
 // Faça um programa que receba um número digitado pelo usuário e informe se o número é primo ou não.
+/*
+main() {
+	 setlocale(LC_ALL, "Portuguese");
+	 int num, i;
+	 int resultado = 0;
+	 
+	 printf("Digite um número: ");
+	 scanf("%d", &num);
+	 
+	 if(num <= 1){
+	 	printf("\nnão é primo\n", num);
+	 	return 0;
+	 }
+	 
+	 for (i = 2; i <= num / 2; i++) {
+	    if (num % i == 0) {
+	       resultado++;
+	       break;
+	    }
+	 }
+	 
+	 if (resultado == 0){
+	 	printf("\n é primo\n", num);
+	 }
+	 else{
+	 	printf("\n não é primo\n", num);
+	 }
+    
+}
+*/
+
 
 // Faça um jogo da forca. O jogador 1 deve cadastrar uma palavra e o jogador 2 deve tentar adivinhar.
 // O jogador 2 deve informar uma letra de cada vez. Se acertar, a letra é revelada nas posições que contém a letra.
