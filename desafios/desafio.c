6616d62212017cab0ef8c591d13675cb27b25994
Pietro Rosolia <102825626+Pietro2704@users.noreply.github.com>
2024-03-21 11:02:56 -0300

    até o 5

--- desafios/desafio.c
+++ desafios/desafio.c
@@ -1,7 +1,9 @@
 #include<stdio.h> //print(), scanf()
 #include<stdlib.h> // system ()
 #include<locale.h> // local
+#include<stdbool.h> // bool
 
+// 1)
 // Fazer um programa no qual o usuário digite o início e o fim de um intervalo e o programa deve exibir todos os números ímpares no intervalo. 
 /*
 main(){
@@ -27,14 +29,43 @@ main(){
 
 
 
+// 2)
 // Fazer um programa que force o usuário a digitar um número par para finalizar o programa. 
 // Enquanto o usuário estiver digitando um número ímpar
 // o programa fica repetindo a mensagem "Digite um número par para finalizar".
+/*
+bool Impar(int numero){
+	if(numero % 2 == 0){
+		return false;
+	}
+	return true;
+}
+bool Par(int numero){
+	if(numero % 2 == 0){
+		return true;
+	}
+	return false;
+}
+main(){
+	setlocale(LC_ALL,"Portuguese");
+	int num;
+	do{
+		printf("Digite um número para finalizar: ");
+		scanf("%i", &num);	
+	}while(Impar(num));
+	
+	if(Par(num)){
+		printf("Programa encerrado!");
+		return 0;
+	}
+	
+}
+*/
 
 
 
 
-
+// 3)
 // Faça um programa que exiba a tabuada até 10 de um número digitado pelo usuário.
 /*
 main(){
@@ -60,7 +91,7 @@ main(){
 
 
 
-
+// 4)
 // Faça um programa que peça para o usuário digitar um número,
 // de acordo com o número digitado, o programa deve exibir a frase bom dia "n" vezes, conforme o número digitado pelo usuário.
 /*
@@ -81,7 +112,7 @@ main(){
 
 
 
-
+// 5)
 // Faça um programa que receba um número digitado pelo usuário e informe se o número é primo ou não.
 /*
 main() {
@@ -115,6 +146,10 @@ main() {
 */
 
 
+
+
+
+// 6)
 // Faça um jogo da forca. O jogador 1 deve cadastrar uma palavra e o jogador 2 deve tentar adivinhar.
 // O jogador 2 deve informar uma letra de cada vez. Se acertar, a letra é revelada nas posições que contém a letra.
 // Se errar, a letra deve ser salva num vetor que armazenará os erros. O jogador 2 pode errar 5 vezes.
